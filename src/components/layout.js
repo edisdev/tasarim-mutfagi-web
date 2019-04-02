a86d7ecbc6b8966c170fd88d31035383c7a5c2ff
Adem Ilter <ademilter@gmail.com>
2019-04-03 00:08:01 +0300

    added style-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from '../utils/globalStyle'
+import theme from '../utils/theme'
 
 import Header from './header'
 
@@ -16,24 +20,27 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </>
+      </ThemeProvider>
     )}
   />
 )
---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+export default {
+  color: {
+    white: '#ffffff',
+    dark: '#262626',
+    gray: '#a8a8a8'
+  },
+  font: {
+    family: 'Inter',
+    size: {
+      normal: 18,
+      h1: 48,
+      h2: 32
+    },
+    weight: {
+      light: 300,
+      regular: 400,
+      semibold: 600,
+      bold: 800
+    }
+  }
+}
