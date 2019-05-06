0a63bdc41c52aa7a7f48621b1e180584cf9a82c3
Adem Ilter <ademilter@gmail.com>
2019-05-06 17:29:49 +0300

    Fix SectionSupport mobile screen

--- src/components/SectionSupport/container.js
+++ src/components/SectionSupport/container.js
@@ -8,26 +8,28 @@ function Event({ supporters }) {
       <div className="container">
         <h2>Destekçilerimiz</h2>
 
-        <div className="row justify-content-center mt-4">
-          {supporters.map(support => {
-            return (
-              <div key={support.id} className="col-2">
-                {/* TODO: hover? */}
-                <a
-                  rel="noopener noreferrer"
-                  className="SectionSupport-logo"
-                  href={support.url}
-                >
-                  <img
-                    alt={support.label}
-                    src={require(`../../images/supporters/${
-                      support.logoName
-                    }.svg`)}
-                  />
-                </a>
-              </div>
-            )
-          })}
+        <div className="SectionSupport-list mt-4">
+          <div className="row">
+            {supporters.map(support => {
+              return (
+                <div key={support.id} className="col-6 col-lg-2">
+                  {/* TODO: hover? */}
+                  <a
+                    rel="noopener noreferrer"
+                    className="SectionSupport-logo"
+                    href={support.url}
+                  >
+                    <img
+                      alt={support.label}
+                      src={require(`../../images/supporters/${
+                        support.logoName
+                      }.svg`)}
+                    />
+                  </a>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/components/SectionWhy/container.js
+++ src/components/SectionWhy/container.js
@@ -10,8 +10,8 @@ function Why({ data }) {
         <div className="row">
           <div className="col-12">
             <h2>Neden Katılmalıyım?</h2>
-            <div className="SectionWhy-list">
-              <div className="row mt-5">
+            <div className="SectionWhy-list mt-5">
+              <div className="row">
                 {data.map(item => (
                   <div key={item.id} className="col-10 col-sm-6 col-lg-4">
                     <WhyCard {...item} />
