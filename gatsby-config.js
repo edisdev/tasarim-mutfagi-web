a70a249a08ba95884b661f0848bd2b026acb5fab
Adem Ilter <ademilter@gmail.com>
2019-05-21 17:29:50 +0300

    cms init

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,21 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 200
+            }
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`
+        ]
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
---
+++ src/data/testimonials/index.md
@@ -0,0 +1,6 @@
+---
+name: "Yakup Bayrak"
+title: "Kurucu @Sherpa"
+photo: "./user.jpg"
+---
+Successful businesses have many things in common, today we’ll look at the big ‘R’of recognition and how a digital advertising network may help.
