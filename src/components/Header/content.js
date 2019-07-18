282da8931ed4e12f846fd3ea739b66b48205dd71
Adem Ilter <ademilter@gmail.com>
2019-07-18 17:42:58 +0300

    header fix

--- src/components/Header/content.js
+++ src/components/Header/content.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import c from 'classnames'
 
 import './style.css'
@@ -6,40 +6,54 @@ import logo from '../../images/logo.svg'
 import Navigation from '../Navigation'
 import MobileMenuToggle from '../MobileMenuToggle'
 
-class Header extends Component {
-  state = {
-    isShow: false
-  }
+function Header({ data }) {
+  const [isShow, setShow] = useState(false)
+  let scrollY = 0
 
-  onToggle = () => {
-    this.setState(prevState => ({
-      isShow: !prevState.isShow
-    }))
-  }
-
-  render() {
-    const { isShow } = this.state
-    const { data } = this.props
-
-    return (
-      <>
-        <header className={c('Header', { 'mobile-menu-show': isShow })}>
-          <div className="Header-body">
-            <div className="container">
-              <img src={logo} alt="Tasarım Mutfağı" />
-              <MobileMenuToggle onToggle={this.onToggle} isActive={isShow} />
-              <Navigation data={data} />
-            </div>
-          </div>
-          <div className="Header-menu">
-            <div className="container">
-              <Navigation data={data} isMobile={true} />
-            </div>
-          </div>
-        </header>
-      </>
+  if (typeof window !== 'undefined') {
+    const [bodyOffset, setBodyOffset] = useState(
+      document.body.getBoundingClientRect()
     )
+    const [scrollY, setScrollY] = useState(bodyOffset.top)
+    console.log(scrollY)
+
+    const listener = () => {
+      setBodyOffset(document.body.getBoundingClientRect())
+      setScrollY(-bodyOffset.top)
+    }
+
+    useEffect(() => {
+      window.addEventListener('scroll', listener)
+      return () => {
+        window.removeEventListener('scroll', listener)
+      }
+    })
   }
+
+  const onToggle = () => setShow(!isShow)
+
+  return (
+    <header
+      className={c(
+        'Header',
+        { 'mobile-menu-show': isShow },
+        { 'on-scroll': scrollY > (160 - 90) / 2 }
+      )}
+    >
+      <div className="Header-body">
+        <div className="container">
+          <img src={logo} alt="Tasarım Mutfağı" />
+          <MobileMenuToggle onToggle={onToggle} isActive={isShow} />
+          <Navigation data={data} />
+        </div>
+      </div>
+      <div className="Header-menu">
+        <div className="container">
+          <Navigation data={data} isMobile={true} />
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
